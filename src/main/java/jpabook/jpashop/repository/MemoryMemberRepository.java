61f5c59a775f1e027039c41323161f3f03dcb80a
Jiseok-Hong <ji385@naver.com>
2022-02-01 00:30:30 +0800

    Create Spring bin

--- src/main/java/jpabook/jpashop/repository/MemoryMemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemoryMemberRepository.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
+
 
 public class MemoryMemberRepository implements MemberRepository {
 
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -3,12 +3,16 @@ package jpabook.jpashop.service;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public class MemberService {
     private final MemberRepository memberRepository;
+
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
